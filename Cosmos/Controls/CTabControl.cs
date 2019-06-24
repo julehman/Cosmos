b8ce506ce24e47f82288e61998e42108488a12b8
Tom Unger <u.tom.2000@web.de>
2019-06-24 14:52:37 +0200

    TabControl (unfinished) & TabItem

---
+++ Cosmos/Controls/CTabControl.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Cosmos
+{
+    public class CTabControl : TabControl
+    {
+        static CTabControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CTabControl), new FrameworkPropertyMetadata(typeof(CTabControl)));
+        }
+    }
+}

430c3da367dfe4a9af7cadee2ede305e6a4d0d39
Tom Unger <u.tom.2000@web.de>
2019-08-14 10:22:59 +0200

    textboxblock, readonly / enabled textbox

---
+++ Cosmos/Controls/CTextBoxBlock.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace Cosmos
+{
+    public class CTextBoxBlock : CTextBox
+    {
+        static CTextBoxBlock()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CTextBoxBlock), new FrameworkPropertyMetadata(typeof(CTextBoxBlock)));
+        }
+
+    }
+
+}
